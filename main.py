10162937ae447dc0c83626a7a49e424e41dc0137
SamWooHK <72465151+SamWooHK@users.noreply.github.com>
2023-01-06 21:33:53 +0900

    use streamlit extras

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import streamlit as st
+from streamlit_extras.dataframe_explorer import dataframe_explorer
 import pandas as pd
 import numpy as np
-from io import StringIO
-import math
-import time
 import os
 import plotly.express as px
+from sklearn.preprocessing import StandardScaler
 
+
+sc = StandardScaler()
 #page setting
 st.set_page_config(page_title="Simple Data Manipulation",page_icon="📔",layout="wide")
 
 
 ##functions
 #upload file
-@st.cache
+@st.cache(allow_output_mutation=True)
 # @st.experimental_memo
 def load_file(file,sep):
     file_ext = os.path.splitext(file.name)[1]
@@ -26,8 +27,7 @@ def load_file(file,sep):
         print(len(df.columns))
     elif file_ext == ".xlsx":
         df = pd.read_excel(file, index_col=0)
-
-        
+       
     return df
 
 #Rename headers
@@ -96,74 +96,73 @@ with st.container():
             st.write(dataframe)
          
         # #Result  
-        "## Modify Data" 
-        
-        # #Choose the needed columns
+        "## Modify Data"
         data_headers = dataframe.columns.to_list()
+        headers_needed = st.multiselect("Headers filter (default first 5 columns)",data_headers,default=data_headers[:5])
+                
+        #Choose the needed columns
+        
+        filtered_df = dataframe_explorer(dataframe[headers_needed])
       
-        with st.expander("Make amendments",expanded=True):
-                               
-            headers_needed = st.multiselect("Headers filter (default first 5 columns)",data_headers,default=data_headers[:5])
+        with st.expander("Show Modified Data",expanded=True):
             
-            #Rename headers
-            change_headers(dataframe,headers_needed)
-            
-            # query
-            with st.form("query",clear_on_submit=False):
-                query_selected = {} 
-                data_selection = dataframe  
-                
-                for header in headers_needed:
-
-                    title = st.multiselect(
-                    f"Select the {header}:",
-                    options=dataframe[header].unique(),
-                    default=dataframe[header].unique())
-                    query_selected[header] = title
-                    
-                query_con = " & ".join(f"{key} == {value}" for key,value in query_selected.items())    
-                add_query = st.form_submit_button("Query")
-                if add_query:
-                    print(query_con)
-                    data_selection = dataframe.query(query_con)
-
-            
-                
-                           
-             
-        #Show details
-        "## Result"
-        with st.container():
-            
-            modified_df = data_selection[headers_needed].query(query_con)
             headers, rows,_,_,_ = st.columns(5)
-            headers.metric(label="Headers",value=len(modified_df.columns))
-            rows.metric(label="Rows",value=len(modified_df))
-
-            try:
-                st.write(modified_df)
-            except NameError:
-                modified_df = dataframe.query(query_con)
-                st.write(modified_df)
+            headers.metric(label="Headers",value=len(filtered_df.columns))
+            rows.metric(label="Rows",value=len(filtered_df))
             
-            #Download as excel, json
-            @st.cache
-            def convert_df(df):
-                # IMPORTANT: Cache the conversion to prevent computation on every rerun
-                return df.to_csv().encode('utf-8')
+            st.dataframe(filtered_df, use_container_width=True) 
+            
+        #     #Rename headers
+        #     change_headers(dataframe,headers_needed)
+            
+        #     # query
+        #     with st.form("query",clear_on_submit=False):
+        #         query_selected = {} 
+        #         data_selection = dataframe  
+                
+        #         for header in headers_needed:
 
-            csv = convert_df(modified_df)
+        #             title = st.multiselect(
+        #             f"Select the {header}:",
+        #             options=dataframe[header].unique(),
+        #             default=dataframe[header].unique())
+        #             query_selected[header] = title
+                    
+        #         query_con = " & ".join(f"{key} == {value}" for key,value in query_selected.items())
+        #         add_query = st.form_submit_button("Query")
+        #         if add_query:
+        #             print(query_con)
+        #             data_selection = dataframe.query(query_con)
+      
+        #Show details
+        # "## Result"
+        # with st.container():
+            
+        #     # modified_df = data_selection[headers_needed].query(query_con)
+        #     headers, rows,_,_,_ = st.columns(5)
+        #     headers.metric(label="Headers",value=len(filtered_df.columns))
+        #     rows.metric(label="Rows",value=len(filtered_df))
+            
+        #     st.write(filtered_df)
+            
+        #     #Download as excel, json
+        #     @st.cache
+        #     def convert_df(df):
+        #         # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        #         return df.to_csv().encode('utf-8')
 
-            st.download_button(
-                label="Download result as CSV",
-                data=csv,
-                file_name='modified_df.csv',
-                mime='text/csv',
-            )           
+        #     csv = convert_df(filtered_df)
+
+        #     st.download_button(
+        #         label="Download result as CSV",
+        #         data=csv,
+        #         file_name='modified_df.csv',
+        #         mime='text/csv',
+        #     )           
         
         with st.expander("Plot Graph - still testing",expanded=False): 
             "## Plot Graph"
-            groupby = st.multiselect("Groupby (default=mean)",headers_needed,default=None,key=2)
+            groupby = st.multiselect("Groupby (default=mean)",filtered_df.columns,default=None,key=2)
                 
             Agg = st.text_area("Aggregations if any: ",placeholder="e.g.: a:mean,b:sum,c:size")
             
@@ -182,22 +181,18 @@ with st.container():
             with st.container():
                 
                 if len(groupby)==0:
-                    graph_df = modified_df
+                    graph_df = filtered_df
                 else:
                     if len(agg_dict) == 0:
-                        graph_df = modified_df.groupby(groupby, as_index=False).mean(numeric_only=False)
+                        graph_df = filtered_df.groupby(groupby, as_index=False).mean(numeric_only=False)
                     else:
-                        graph_df = modified_df.groupby(groupby, as_index=False).agg(agg_dict)
+                        graph_df = filtered_df.groupby(groupby, as_index=False).agg(agg_dict)
                 
                 "### Graph Data"
                 #Show details
                 headers, rows,_,_,_ = st.columns(5)
                 headers.metric(label="Columns",value=len(graph_df.columns))
                 rows.metric(label="Rows",value=len(graph_df))  
-                
-                
-                #Rename headers
-                # graph_df = change_headers(graph_df,graph_df.columns.to_list())
                             
                 st.write(graph_df)
                 
@@ -217,13 +212,13 @@ with st.container():
                 elif graph_type == graph_list[1]:
                     graph.area_chart(graph_df,x=x_title,y=y_title, width=0, height=400)
                 elif graph_type == graph_list[2]:
-    
+
                     graph.bar_chart(graph_df,x=x_title,y=y_title, width=0, height=400)
                     
-                    title = f"{x_title} - {y_title} graph"
-                    fig = px.bar(graph_df, x=x_title, y=graph_df[y_title], color=y_title, title=title)
-                    graph.plotly_chart(fig)
+                    # title = f"{x_title} - {y_title} graph"
+                    # fig = px.bar(graph_df, x=x_title, y=graph_df[y_title], color=y_title, title=title)
+                    # graph.plotly_chart(fig)
             
 #footer
 with st.container():
-    "Made by Sam Woo 2022"
+    "Made by  &copy;Sam Woo 2023"
